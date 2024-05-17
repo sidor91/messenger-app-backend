94c4bb6d88ff4aee46b9e74a0251c74a4b5ea6e2
sidor91 <sidorchik91@gmail.com>
2024-05-17 14:05:00 +0200

    ADD | main.ts config

---
+++ src/app-config/app-config.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import databaseConfig from './config/database.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService) => ({ ...configService.get('typeorm') }),
+    }),
+  ],
+})
+export class AppConfigModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
+import { AppConfigModule } from './app-config/app-config.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import DatabaseConfig from './config/database.config';
 import { UserModule } from './services/user/user.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [DatabaseConfig],
-    }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService) => ({ ...configService.get('typeorm') }),
-    }),
-    UserModule,
-  ],
+  imports: [UserModule, AppConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
