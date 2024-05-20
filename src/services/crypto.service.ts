093da0a408417abb3e6ad2967d1fdcd13e2850d1
sidor91 <sidorchik91@gmail.com>
2024-05-20 11:49:48 +0200

    ADD | crypto service

---
+++ src/services/crypto.service.ts
@@ -0,0 +1,22 @@
+import * as bcrypt from 'bcrypt';
+
+export interface CryptoServiceI {
+  hashPassword(password: string): Promise<string>;
+  validatePassword(password: string, hash: string): Promise<boolean>;
+}
+
+class CryptoService implements CryptoServiceI {
+  private readonly saltRounds: number;
+  constructor() {
+    this.saltRounds = Number(process.env.SALT_ROUNDS);
+  }
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, this.saltRounds);
+  }
+
+  async validatePassword(password: string, hash: string) {
+    return await bcrypt.compare(password, hash);
+  }
+}
+
+export default new CryptoService()
