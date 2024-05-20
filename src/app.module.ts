14297286cc52b1c4dad55161c4f4d9e140acb6ab
sidor91 <sidorchik91@gmail.com>
2024-05-20 20:09:40 +0200

    ADD | chat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
-  imports: [UserModule, AppConfigModule, AuthModule],
+  imports: [UserModule, AppConfigModule, AuthModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
