f370d7e7b64ccf13b4b868fc7546014a1eed23b8
sidor91 <sidorchik91@gmail.com>
2024-05-31 19:51:53 +0200

    change the notification resource folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,8 @@ import { AppConfigModule } from './config/app-config.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ChatModule } from './modules/chat/chat.module';
 import { MessageModule } from './modules/message/message.module';
+import { NotificationsModule } from './modules/notifications/notifications.module';
 import { UserModule } from './modules/user/user.module';
-import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [
