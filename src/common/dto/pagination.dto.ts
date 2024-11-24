6fa556bec0bd6e4ac9cd4ceb7ccb966d3c7824d7
sidor91 <sidorchik91@gmail.com>
2024-11-24 14:41:36 +0100

    Create pagination.dto.ts

---
+++ src/common/dto/pagination.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumberString, IsOptional } from "class-validator";
+
+export class PaginationDto {
+  @ApiProperty({
+    example: 1,
+    description: 'pagination page',
+    required: false,
+  })
+  @IsNumberString()
+  @IsOptional()
+  page: string;
+
+  @ApiProperty({
+    example: 10,
+    description: 'number of items per page',
+    required: false,
+  })
+  @IsNumberString()
+  @IsOptional()
+  limit: string;
+}
