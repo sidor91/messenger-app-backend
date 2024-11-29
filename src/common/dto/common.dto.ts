fe6502b633b502b968dbd1fb682f5abcc2d6dfb0
sidor91 <sidorchik91@gmail.com>
2024-11-29 20:36:45 +0100

    Update common.dto.ts (made all fields unnecessary)

--- src/common/dto/common.dto.ts
+++ src/common/dto/common.dto.ts
@@ -5,17 +5,17 @@ export class CommonDto {
     example: '2312ff46-d975-488d-a862-941568e0e158',
     description: 'unique id',
   })
-  id: string;
+  id?: string;
 
   @ApiProperty({
     example: '2024-05-20 16:42:02.966148+03',
     description: 'created at',
   })
-  created_at: Date;
+  created_at?: Date;
 
   @ApiProperty({
     example: '2024-05-20 16:42:02.966148+03',
     description: 'updated at',
   })
-  updated_at: Date;
+  updated_at?: Date;
 }
